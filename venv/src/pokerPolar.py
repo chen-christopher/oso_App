c4e031220469fa7b26d50497e25e160ae35c962e
Mateo Revilla <mateorevillalecca@gmail.com>
2020-11-14 17:44:07 -0500

    Finished logic to set hand

--- venv/src/pokerPolar.py
+++ venv/src/pokerPolar.py
@@ -19,27 +19,30 @@ class Cards:
     def __init__(self, strCards): #strCards is in the following format : "1H,2D,4H" it can be uo to 7 cards in total
         self.cardsList = [] #A list of Card's
         self.parse(strCards)
+        self.hand: Hand
 
     def parse(self, strCards):
         strCardsList = strCards.split(",")
         for strCard in strCardsList:
-            number = strCard[0]
+            number = int(strCard[0])
             suit = strCard[1]
             card = Card(number, suit)
             self.cardsList.append(card)
-
-    def toNumbers(self):
-        return map(lambda x: card.number, self.cardsList) 
-
-    def toSuits(self):
-        return map(lambda x: card.suit, self.cardsList) 
+        self.hand = setHand(self.cardsList)
+        self.hand.output()
 
 #HAND
 class Hand:
-    def __init__(self, cards):
+    def __init__(self, cards, pattern, topCards):
         self.cards = cards
-        self.pattern = Pattern.HighCard #The Pattern, set to the lowest by default
-        self.topCards = [] #TOP 5 cards
+        self.pattern = pattern
+        self.topCards = topCards
+    
+    def output(self):
+        print("PATTERN: " + str(self.pattern))
+        print("TOP CARDS:")
+        for card in self.topCards:
+            print(str(card.number) + card.suit)
 
 
 
@@ -67,16 +70,13 @@ oso.load_file("poker.polar")
 
 
 #-------------------------FUNCTIONS---------------------------
-'''
-def setHand(cards): #Establish the pattern and the top 5 cards
 
+def setHand(cards): #Establish the pattern and the top 5 cards
     fiveCards = []
-    pattern = Pattern.HighCard
 
     #POLAR QUERIES
-    queryNumberCount = list(oso.query_rule("count", cards.toNumbers, Variable("elem"), Variable("count"))) 
-    querySuitsCount = list(oso.query_rule("count", cards.toSuits, Variable("elem"), Variable("count")))
-
+    queryNumberCount = list(oso.query_rule("count", toNumbers(cards), Variable("elem"), Variable("count"))) 
+    querySuitsCount = list(oso.query_rule("count", toSuits(cards), Variable("elem"), Variable("count")))
 
     #SORTING CARDS FOR NUMBER QUERY
     single = []
@@ -104,118 +104,234 @@ def setHand(cards): #Establish the pattern and the top 5 cards
     for res in querySuitsCount:
         element = res["bindings"]["elem"]
         count = res["bindings"]["count"]
-
         if count >= 5:
             flush = element
 
+    
+   
 
 
+    #CHECK FOR HAND HIERARCHY CASCADE
+
+    #Check for straight flush 
+
+    if flush is not None:
+        sameSuit = outputCardsWithSuit(cards, flush)
+        straightcards = checkForStraight(sameSuit)
+        
+        if len(straightcards) > 0:
+            fiveCards = straightcards
+            pattern = Pattern.StraightFlush
+            return Hand(cards, pattern, fiveCards)
 
     
-
-    #Check for straight flush in singles
-
-    #----IMPLEMENT
 
     #Check for quads
     
     if len(quads) == 1:
-        fiveCards.append(outputCardsWithNumber(cards, quads[0])) #Append quads
-        fiveCards.append(outputCardsWithNumber(helperPokerSortDecreasing(pair + trio + single))[0]) #Append first element of sorted(singles + pair + trio)
+        fiveCards += outputCardsWithNumber(cards, quads[0]) #Append quads
+        join = pair + trio + single
+        fiveCards.append(outputCardsWithNumber(cards, helperPokerSortDecreasing(join)[0])[0]) #Append first element of sorted(singles + pair + trio)
         pattern = Pattern.Poker
+        return Hand(cards, pattern, fiveCards)
 
     #Check for FullHouse
 
     if len(trio) == 2:
-        sortedTrios = helperPokerSortDecreasing(sorted)
+        sortedTrios = helperPokerSortDecreasing(trio)
         #append three cards of sortedTrio[0] to fiveCards
-        fiveCards.append(outputCardsWithNumber(sortedTrios[0]))
+        fiveCards += outputCardsWithNumber(cards, sortedTrios[0])
         #append two cards of sortedTrio[2] to fiveCards
-        fiveCards.append(outputCardsWithNumber(sortedTrios[1])[:2])
+        fiveCards += outputCardsWithNumber(cards, sortedTrios[1])[:2]
         pattern = Pattern.FullHouse
-    elif len(trio) == 1 and len(pair) >= 1:
+        return Hand(cards, pattern, fiveCards)
+    if len(trio) == 1 and len(pair) >= 1:
         sortedPair = helperPokerSortDecreasing(pair)
 
         #Append three cards of trio[0]
-        fiveCards.append(outputCardsWithNumber(trio[0]))
+        fiveCards += outputCardsWithNumber(cards, trio[0])
         #Append two cards of sortedPair[0]
-        fiveCards.append(outputCardsWithNumber(sortedPair[0]))
+        fiveCards += outputCardsWithNumber(cards, sortedPair[0])
         pattern = Pattern.FullHouse
+        return Hand(cards, pattern, fiveCards)
     
     #Check for flush 
 
     if flush is not None:
-        sameSuit = outputCardsWithSuit(flush)
-        fiveCards = outputCardsWithNumber(helperPokerSortDecreasing(sameSuit)[:5])
+        sameSuit = toNumbers(outputCardsWithSuit(cards, flush))
+        sameSuit = helperPokerSortDecreasing(sameSuit)
+        for number in sameSuit:
+            fiveCards.append(outputCardsWithNumber(cards, number)[0])
+        fiveCards = fiveCards[:5]
         pattern = Pattern.Flush
+        return Hand(cards, pattern, fiveCards)
 
 
         
     #Check for straight
 
-    3 4 5 6 7 9 10
+    straightCards = checkForStraight(cards)
+    if len(straightCards) != 0:
+            fiveCards = straightCards
+            pattern = Pattern.Straight
+            return Hand(cards, pattern, fiveCards)
 
-    1 2 3 3 4 5 7
-    
-    2 3 3 4 5 7 13
-    
-    2 3 4 5 6
-
-    sortedOne = cards.toNumbers.sort(reverse = True)
-    temp = sortedOne
-
-    print(list(oso.query_rule("straight", sortedOne, temp)))
 
     #Check for Trio
 
     if len(trio) == 1:
         sortedSingles = helperPokerSortDecreasing(single)
         #Append three cards of trio[0] to tfiveCards
-        fiveCards.append(outputCardsWithNumber(trio[0]))
+        fiveCards += outputCardsWithNumber(cards, trio[0])
         #Append sortedSingle[0 and 1] to fiveCards
-        fiveCards.append(outputCardsWithNumber(sortedSingles[0]))
-        fiveCards.append(outputCardsWithNumber(sortedSingles[1]))
+        fiveCards += outputCardsWithNumber(cards, sortedSingles[0:2])
+        pattern = Pattern.Trio
+        return Hand(cards, pattern, fiveCards)
         
 
     #Check for Pair
 
     sortedPair = helperPokerSortDecreasing(pair)
-
+    sortedSingles = helperPokerSortDecreasing(single)
     if len(sortedPair) == 1:
         #Append sorted pair to five cards
+        fiveCards += outputCardsWithNumber(cards, sortedPair[0])
         #Append Sorted singles three cards
+        fiveCards += outputCardsWithNumber(cards, sortedSingles[0:3])
+        pattern = Pattern.Pair
+        return Hand(cards, pattern, fiveCards)
+
     if len(sortedPair) == 2:
         #Append two sorted pair to five cards
+        sortedPair = helperPokerSortDecreasing(pair)
+        fiveCards += outputCardsWithNumber(cards, sortedPair[0])
+        fiveCards += outputCardsWithNumber(cards, sortedPair[1])
         #Append one sorted single to five cards
-    if len(sortedPair == 3):
+        fiveCards.append(outputCardsWithNumber(cards, sortedSingles[0])[0])
+        pattern = Pattern.TwoPair
+        return Hand(cards, pattern, fiveCards)
+    if len(sortedPair) == 3:
         #Append first two sorted pair to five cards
-        #singlesAndLowPair = sorted(singles + sortedPair[2])
+        sortedPair = helperPokerSortDecreasing(pair)
+        fiveCards += outputCardsWithNumber(cards, sortedPair[0])
+        fiveCards += outputCardsWithNumber(cards, sortedPair[1])
         #Append first card of singlesAndLowPair
+       
+        singlesAndLowPair = helperPokerSortDecreasing(sortedSingles + [sortedPair[2]])
+        fiveCards.append(outputCardsWithNumber(cards, singlesAndLowPair[0])[0])
+        pattern = Pattern.TwoPair
+        return Hand(cards, pattern, fiveCards)
+
+    
+    #HighCards
+    fiveCards = sortedSingles[0:5]
+    pattern = Pattern.HighCard
+    return Hand(cards, pattern, fiveCards) 
+        
 
 
-'''
 
 
 
 
 
-def helperPokerSortDecreasing(list):  #Sorts in decreasing order taking into consideration 1 being the highest
-    sorted = list.sort(reverse= True)
-    if sorted.contains(1):
-        sorted.instert(0, sorted.pop())
-    return sorted
+def toNumbers(cards):
+    numbers = []
+    for card in cards:
+        numbers.append(card.number)
+    return numbers
+
+def toSuits(cards):
+    suits = []
+    for card in cards:
+        suits.append(card.suit)
+    return suits
+
+
+def helperPokerSortDecreasing(numbers):  #Sorts in decreasing order taking into consideration 1 being the highest
+    ones = []
+    rest =[]
+    for number in numbers:
+        if number == 1:
+            ones.append(number)
+        else:
+            rest.append(number)
+    rest.sort(reverse= True)
+    final = ones + rest
+    return final
 
 
 def outputCardsWithNumber(cards, number):
-    return filter(lambda x: card.number == number , cards)
+    newCards = []
+    for card in cards:
+        if card.number == number:
+            newCards.append(card)
+    return newCards
+    
 
 def outputCardsWithSuit(cards, suit):
-    return filter(lambda x: card.suit == suit , cards)
+    newCards = []
+    for card in cards:
+        if card.suit == suit:
+            newCards.append(card)
+    return newCards
+
+def checkForStraight(cards):
+    uniqueNumbers = []
+    uniqueCards = []
+    for card in cards:
+        if card.number not in uniqueNumbers:
+            uniqueNumbers.append(card.number)
+            uniqueCards.append(card)
+    if (len(uniqueNumbers) < 5):
+        return []
+        
+    firstArray = uniqueNumbers
+    firstArray.sort(reverse = True)
+    secondArray = []
+    for number in uniqueNumbers:
+        if number == 1:
+            secondArray.append(13)
+        else:
+            secondArray.append(number)
+    top = max(straightHelper(firstArray), straightHelper(secondArray))
+    print(top)
+    if top == 0:
+        return []
+    
+    sortedCards = []
+
+    index = 0
+    while (index < 5):
+        number = top - index
+        if (number == 13):
+            number = 1
+        card = outputCardsWithNumber(uniqueCards, number)[0]
+        sortedCards.append(card)
+        index += 1
+    return  sortedCards
+    
+
+def straightHelper(numbers):
+    temp1 = numbers.copy()
+    temp2 = numbers.copy()
+    
+    straightQuery = list(oso.query_rule("straight", temp1, temp2, Variable("top")))
+    if len(straightQuery) == 0:
+        return 0
+    else:
+        return straightQuery[0]['bindings']['top']
+
+
+
+
+
     
     
 
-temp1 = [7,5,4,3,3,2,1]
-temp2 = [7,5,4,3,3,2,1]
-print("STRAIGHT")
-lis = list(oso.query_rule("straight", temp1, temp2, Variable("top")))
-print(lis[0]['bindings']['top'])
+#TEST
+
+cards = Cards("1H,2H,4D,2D,3D,5H")
+cards = Cards("1H,1D,4D,1T,1C,5H")
+cards = Cards("5H,1D,4D,1T,4C,5H")
+
